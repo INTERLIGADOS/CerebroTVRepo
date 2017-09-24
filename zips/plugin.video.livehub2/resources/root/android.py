8b29ec9973fb405750a089c1093fa61322841b67
biglad <biglad@mgawow.co.uk>
2017-09-24 11:52:04 +0100

    add more addons
    woohoo

---
+++ zips/plugin.video.livehub2/resources/root/android.py
@@ -0,0 +1,270 @@
+import xbmc,os
+
+addon_id   = 'plugin.video.livehub'
+
+icon       = xbmc.translatePath(os.path.join('special://home/addons/' + addon_id, 'icon.png'))
+fanart     = xbmc.translatePath(os.path.join('special://home/addons/' + addon_id , 'fanart.jpg'))
+
+def cat():
+	addDir('[COLOR white][B]Geo TV[/COLOR][/B]','geotv',4,'https://image.winudf.com/v2/image/Y29tLnNuci5lbnRfaWNvbl8wX2I0N2VlYjZi/icon.png?w=170&fakeurl=1&type=.png',fanart,'')
+	addDir('[COLOR white][B]eDoctor IPTV[/COLOR][/B]','edoctor',4,'https://lh3.googleusercontent.com/qydRpyUiySg1MuswXdbQE88PBfyPH8uJYhONBL3UO1Ij4yEHtftHFK3pKXHXppFUCmk=w300',fanart,'')
+	addDir('[COLOR white][B]Mobdro[/COLOR][/B]','mobdro',4,'http://apk.co/images/mobdro-2014-freemium.png',fanart,'')
+	addDir('[COLOR white][B]Mobile TV[/COLOR][/B]','mobiletv',4,'https://4.bp.blogspot.com/-mAW-a99Dvlw/WO9Mf16-k6I/AAAAAAAAXTg/z_lXe9-JV2UA3xwrFTkfvSBvmXcXI0imwCLcB/s1600/unnamed.jpg',fanart,'')
+	addDir('[COLOR white][B]Pak India Sports[/COLOR][/B]','pakindiasport',4,'http://www.madhyamam.com/en/sites/default/files/india-pak.jpg',fanart,'')
+	addDir('[COLOR white][B]Swift Streams[/COLOR][/B]','snappystreams',4,'https://image.winudf.com/v2/image/Y29tLnN3aWZ0LnN0cmVhbV9zcmNqdHBicg/icon.png?w=170&fakeurl=1&type=.png',fanart,'')
+	addDir('[COLOR white][B]TV Online Plus+[/COLOR][/B]','tvonlineplus',4,'https://1.bp.blogspot.com/-I_th1gsuna8/WRsJAg3SqMI/AAAAAAAAGdk/J-YznRwYMaMB_Y9ZINmhQzyvopkAbtA1wCLcB/s200/18424147_301931813588370_8617351845985017033_n.jpg',fanart,'')
+def get(url):
+	if url == 'sourceetv':
+		sourcetv()
+	elif url == 'snappystreams':
+		snappystreams()
+	elif 'SnappyStreamz/api.php?cat_id=' in url:
+		snappystreamschans(url)
+	elif url == 'mobdro':
+		mobdro()
+	elif url == 'livetv':
+		livetv()
+	elif url == 'geotv':
+		geotv()
+	elif '78.129.237.5:25461' in url:
+		geotvchans(url)
+	elif 'pakindiasport' in url:
+		pakindiasport()
+	elif url == 'edoctor':
+		edoctor()
+	elif url == 'mobiletv':
+		mobiletv()
+	elif 'mobiletv:' in url:
+		mobiletvchans(url)
+	elif url == 'tvonlineplus':
+		tvonlineplus()
+	elif 'tvonlineplus:' in url:
+		tvonlinepluschans(url)
+		
+def tvonlineplus():
+	import requests,json
+	open = requests.get('http://proxykingpro.com/tvonlineplus/api.php',headers={'User-Agent':'Dalvik/1.6.0 (Linux; U; Android 4.3.1; WT19M-FI Build/JLS36I)'}).text
+	json = json.loads(open)
+	js   = json['LIVETV']
+	for a in js:
+		name = a['category_name']
+		id   = a['cid']
+		icon = a['category_image']
+		addDir(name,'tvonlineplus:'+id,4,'http://proxykingpro.com/tvonlineplus/images/'+icon,fanart,'')
+		
+def tvonlinepluschans(url):
+	import requests,json
+	id  = (url).replace('tvonlineplus:','')
+	
+	url = 'http://proxykingpro.com/tvonlineplus/api.php?cat_id=%s'%id
+	
+	open = requests.get(url,headers={'User-Agent':'Dalvik/1.6.0 (Linux; U; Android 4.3.1; WT19M-FI Build/JLS36I)'}).text
+	json = json.loads(open)
+	js   = json['LIVETV']
+	for a in js:
+		name = a['channel_title']
+		url  = a['channel_url']
+		icon = a['channel_thumbnail']
+
+		addDir(name.encode('ascii', 'ignore'),url.encode('ascii', 'ignore'),9999,'http://proxykingpro.com/tvonlineplus/images/'+icon.encode('ascii', 'ignore'),fanart,'')
+	
+	
+def mobiletv():
+	import urllib
+	addDir('HQ Links','mobiletv:http://sportstv.club/playlist/mobiletv.m3u',4,icon,fanart,'')
+	addDir('Entertainment/Movies Links','mobiletv:'+urllib.quote_plus('http://sportstv.club/playlist/Movies&Entertainment.m3u'),4,icon,fanart,'')
+	addDir('Indian Links','mobiletv:http://sportstv.club/playlist/indian.m3u',4,icon,fanart,'')
+	addDir('2G Links','mobiletv:http://sportstv.club/playlist/2g.m3u',4,icon,fanart,'')
+	addDir('IPL','mobiletv:http://sportstv.club/playlist/iPl2017.m3u',4,icon,fanart,'')
+	
+	
+	
+		
+def mobiletvchans(url):
+	import requests,urllib,re
+	headers = {'User-Agent':'Dalvik/1.6.0 (Linux; U; Android 4.3.1; WT19M-FI Build/JLS36I)',
+		'Accept-Encoding':'gzip'}
+	
+	url     = (url).replace('mobiletv:','')
+	open    = requests.get(url,headers=headers).text
+	regex   = re.compile('#EXTINF:.+?\,(.+?)\n(.+?)\n', re.MULTILINE|re.DOTALL).findall(open)
+	for name,url in regex:
+		addDir(name,url,10,icon,fanart,'')
+
+def edoctor():
+	import re
+	open = OPEN_URL('https://raw.githubusercontent.com/hadjistyllis/myapp/master/TV%20category.m3u')
+	regex = re.compile('#EXTINF:.+?\,(.+?)\n(.+?)\n', re.MULTILINE|re.DOTALL).findall(open)
+	for name,url in regex:
+		if not 'youtube' in name:
+			addDir(name,url,10,icon,fanart,'')
+
+def pakindiasport():
+    import urllib,urllib2,base64,json
+    req = urllib2.Request( base64.b64decode('aHR0cDovL3NtYXJ0ZXJsb2dpeC5jb20vTmV3QXBwcy9QYWtJbmRpYVNwb3J0c0hEL1YxLTAvbWFpbkNvbnRlbnQucGhw') )      
+    req.add_header(base64.b64decode("VXNlci1BZ2VudA=="),base64.b64decode("UGFrJTIwSW5kaWElMjBTcG9ydHMlMjBIRC8xLjAgQ0ZOZXR3b3JrLzc1OC4wLjIgRGFyd2luLzE1LjAuMA==")) 
+    req.add_header(base64.b64decode("QXV0aG9yaXphdGlvbg=="),base64.b64decode("QmFzaWMgYWtGM1lURXdjenAwZHpGdWEyd3pRbUZ1UVc1Qk5qZzM=")) 
+    response = urllib2.urlopen(req)
+    link=response.read()
+    from resources.modules import rc
+    cryptor=rc.RNCryptor()
+    d=base64.b64decode(link)    
+    decrypted_data = cryptor.decrypt(d, base64.b64decode("YkFuZ3I0bDF0dGwzNTY3"))
+    
+    decrypted_data=json.loads(decrypted_data)
+    dataUrl=decrypted_data[0]["dataUrl"]
+
+    req = urllib2.Request( dataUrl)      
+    req.add_header(base64.b64decode("VXNlci1BZ2VudA=="),base64.b64decode("UGFrJTIwSW5kaWElMjBTcG9ydHMlMjBIRC8xLjAgQ0ZOZXR3b3JrLzc1OC4wLjIgRGFyd2luLzE1LjAuMA==")) 
+    req.add_header(base64.b64decode("QXV0aG9yaXphdGlvbg=="),base64.b64decode("QmFzaWMgYWtGM1lURXdjenAwZHpGdWEyd3pRbUZ1UVc1Qk5qZzM=")) 
+    response = urllib2.urlopen(req)
+    link=response.read()
+
+    d=base64.b64decode(link)    
+    decrypted_data = cryptor.decrypt(d, base64.b64decode("YkFuZ3I0bDF0dGwzNTY3"))
+    
+    jsondata=json.loads(decrypted_data)
+    for a in jsondata:#Cricket#
+	
+                name=a["channelName"]
+                
+                url =a["channelLink"]+'|User-Agent=AppleCoreMedia/1.0.0.13A452 (iPhone; U; CPU OS 9_0_2 like Mac OS X; en_gb)' 
+				
+                icon=a["categoryLogo"]
+                addDir(name,urllib.quote_plus(url),9999,urllib.quote_plus(icon),fanart,'')
+		
+def geotv():
+	import base64,requests,urllib
+	open = requests.get('http://78.129.237.5:25461/enigma2.php?username=geoap&password=apkgeo&type=get_live_categories',headers={'User-Agent':'Dalvik/1.6.0 (Linux; U; Android 4.3.1; WT19M-FI Build/JLS36I)'}).text
+	all_cats = regex_get_all(open,'<channel>','</channel>')
+	for a in all_cats:
+		name = regex_from_to(a,'<title>','</title>')
+		name = base64.b64decode(name)
+		url1  = regex_from_to(a,'<playlist_url>','</playlist_url>').replace('<![CDATA[','').replace(']]>','')
+		if not 'UK/USA/CAN' in name:
+			if not 'MALAY/THAI' in name:
+				if not 'NEPALI' in name:
+					addDir(name,urllib.quote_plus(url1),4,'https://image.winudf.com/v2/image/Y29tLnNuci5lbnRfaWNvbl8wX2I0N2VlYjZi/icon.png?w=170&fakeurl=1&type=.png',fanart,'')
+		
+def geotvchans(url):
+	import re,requests,base64,urllib
+	open = requests.get(url,headers={'User-Agent':'Dalvik/1.6.0 (Linux; U; Android 4.3.1; WT19M-FI Build/JLS36I)'}).text
+	all_cats = regex_get_all(open,'<channel>','</channel>')
+	for a in all_cats:
+		log(a)
+		name = regex_from_to(a,'<title>','</title>')
+		name = base64.b64decode(name)
+		name = re.sub('\[.*?min ','-',name)
+		thumb= regex_from_to(a,'<desc_image>','</desc_image>').replace('<![CDATA[','').replace(']]>','')
+		url1  = regex_from_to(a,'<stream_url>','</stream_url>').replace('<![CDATA[','').replace(']]>','')
+		desc = regex_from_to(a,'<description>','</description>')
+		addDir(name,urllib.quote_plus(url1+'|User-Agent=geoapk'),9999,thumb,fanart,base64.b64decode(desc))
+		
+def livetv():
+	import re
+	open = OPEN_URL('http://163.172.89.151:25461/get.php?username=iptvrestream.net&password=wC5Qtu9Zbl&type=m3u&output=ts')
+	regex = re.compile('#EXTINF:.+?\,(.+?)\n(.+?)\n', re.MULTILINE|re.DOTALL).findall(open)
+	for name,url in regex:
+		addDir(name,url,10,icon,fanart,'')
+		
+		
+def mobdro():
+        import re
+        file = xbmc.translatePath('special://home/addons/plugin.video.livehub2/resources/')
+        if os.path.exists(file):
+            file = open(os.path.join(file, 'mobdrochans.txt'))
+            data = file.read()
+            file.close()
+			
+            all   = re.compile('\n([^:]+):(mpd://[^\n]+)').findall(data)
+            for name,url in all:
+				addDir('[B][COLOR white]%s[/COLOR][/B]'%name,url,10,'http://geekpeaksoftware.com/wp-content/uploads/2016/10/mobdro.png',fanart,'')
+				
+
+		
+def snappystreams():
+	import json,requests
+	url = 'http://173.212.202.101/SnappyStreamz/api.php'
+	
+	headers = {'Authorization': 'Basic U25hcHB5OkBTbmFwcHlA',
+		'User-Agent': 'Dalvik/1.6.0 (Linux; U; Android 4.4.4; SM-G900F Build/KTU84Q)'}
+		
+	open = requests.session().get(url,headers=headers).text
+	js   = json.loads(open)
+	js   = js['LIVETV']
+	for a in js:
+		name = a['category_name']
+		id   = a['cid']
+		icon = a['category_image']
+		addDir('[B][COLOR white]%s[/COLOR][/B]'%name,'http://173.212.202.101/SnappyStreamz/api.php?cat_id='+id,4,'http://swiftstreamz.com/SwiftStream/images/thumbs' + icon,fanart,'')
+		
+		
+def snappystreamschans(url):
+	import json,requests
+
+	headers = {'Authorization': 'Basic U25hcHB5OkBTbmFwcHlA',
+		'User-Agent': 'Dalvik/1.6.0 (Linux; U; Android 4.4.4; SM-G900F Build/KTU84Q)'}
+		
+	open = requests.session().get(url,headers=headers).text
+	js   = json.loads(open)
+	js   = js['LIVETV']
+	for a in js:
+		name = a['channel_title']
+		url  = a['channel_url']
+		icon = a['channel_thumbnail']
+		desc = a['channel_desc']
+		addDir('[B][COLOR white]%s[/COLOR][/B]'%name,'snappystreams:'+url,10,'http://swiftstreamz.com/SwiftStream/images/thumbs/' + icon,fanart,desc)
+		
+######################################################################################################
+		
+		
+		
+def regex_from_to(text, from_string, to_string, excluding=True):
+	import re,string
+	if excluding:
+		try: r = re.search("(?i)" + from_string + "([\S\s]+?)" + to_string, text).group(1)
+		except: r = ''
+	else:
+		try: r = re.search("(?i)(" + from_string + "[\S\s]+?" + to_string + ")", text).group(1)
+		except: r = ''
+	return r
+
+
+def regex_get_all(text, start_with, end_with):
+	import re,string
+	r = re.findall("(?i)(" + start_with + "[\S\s]+?" + end_with + ")", text)
+	return r
+
+
+
+def OPEN_URL(url):
+	import requests
+	headers = {}
+	headers['User-Agent'] = 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36'
+	link = requests.session().get(url, headers=headers, verify=False).text
+	link = link.encode('ascii', 'ignore')
+	return link
+	
+logfile    = xbmc.translatePath(os.path.join('special://home/addons/plugin.video.livehub', 'log.txt'))
+
+def log(text):
+	file = open(logfile,"w+")
+	file.write(str(text))
+	
+
+		
+		
+def addDir(name,url,mode,iconimage,fanart,description):
+	import xbmcgui,xbmcplugin,urllib,sys
+	u=sys.argv[0]+"?url="+url+"&mode="+str(mode)+"&name="+urllib.quote_plus(name)+"&iconimage="+urllib.quote_plus(iconimage)+"&description="+urllib.quote_plus(description)
+	ok=True
+	liz=xbmcgui.ListItem(name, iconImage="DefaultFolder.png", thumbnailImage=iconimage)
+	liz.setInfo( type="Video", infoLabels={"Title": name,"Plot":description})
+	liz.setProperty('fanart_image', fanart)
+	if mode==102 or mode==9999:
+		liz.setProperty("IsPlayable","true")
+		ok=xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),url=u,listitem=liz,isFolder=False)
+	else:
+		ok=xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),url=u,listitem=liz,isFolder=True)
+	return ok
+	xbmcplugin.endOfDirectory
