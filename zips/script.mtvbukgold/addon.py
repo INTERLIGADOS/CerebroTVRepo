ec711181dca4873b6b9b39afc59f0d9721f3438b
biglad <biglad@mgawow.co.uk>
2017-09-26 09:00:46 +0100

    update correct file lol
    update correct file lol

--- zips/script.mtvbukgold/addon.py
+++ zips/script.mtvbukgold/addon.py
@@ -42,10 +42,10 @@ def function1():
     xbmc.executebuiltin('PlayMedia("plugin://plugin.video.livehub2/?description&iconimage=http%3a%2f%2fgeekpeaksoftware.com%2fwp-content%2fuploads%2f2016%2f10%2fmobdro.png&mode=10&name=%5bB%5d%5bCOLOR%20white%5dGOLD%5b%2fCOLOR%5d%5b%2fB%5d&url=mpd%3a%2f%2f2c0898822ab3baf7f93bea86648adb26.m3u8")')
 
 def function2():
-    xbmc.executebuiltin('PlayMedia("plugin://plugin.video.ProjectCypher/?url=http%3A%2F%2F178.33.237.97%3A1888%2Flive%2FTZrQjaZKoO%2FryXH1oFWxZ%2F19.ts%7CUser-Agent%3DONLINETVCLIENT_X60000_X25000_X4000MEGA_V1770%26amp%3Bmode%3D12%26quo&mode=12&iconimage=http%3A%2F%2Fwww.goldtv.it%2Fimages%2FLOGOGOLDTV-800-600.png")')
+    xbmc.executebuiltin('PlayMedia("plugin://plugin.video.supremacy/?url=plugin%3A%2F%2Fplugin.video.f4mTester%2F%3Furl%3Dhttp%3A%2F%2Flive.thecableguytv.com%3A25461%2Flive%2Fwade%2Fwade%2F384.ts%26amp%3Bstreamtype%3DTSDOWNLOADER%26amp%3Bname%3DSUPREMACY%0D&mode=12")')
 
 def function3():
-    xbmc.executebuiltin('PlayMedia("plugin://script.module.streamhublive/?url=swift:http://185.21.217.33:9091/routernew/Gold/playlist.m3u8&mode=10&name=%5BB%5D%5BCOLOR+white%5DGold%5B%2FCOLOR%5D%5B%2FB%5D&iconimage=http%3A%2F%2Fswiftstreamz.com%2FSwiftStream%2Fimages%2Fthumbs%2F28932_gold.jpg&description=Gold+Gold+Gold+Gold+GoldGold")')
- 
+    xbmc.executebuiltin('PlayMedia("plugin://script.module.streamhublive/?url=swift:http://185.21.217.33:9091/routernew/Gold/playlist.m3u8&mode=10&name=%5BB%5D%5BCOLOR+white%5DGold%5B%2FCOLOR%5D%5B%2FB%5D&iconimage=http%3A%2F%2Fswiftstreamz.com%2FSwiftStream%2Fimages%2Fthumbs%2F1648_28932_gold.jpg&description=GoldGoldGoldGoldGoldGoldGoldGoldGoldGoldGoldGoldGoldGoldGoldGoldGoldGoldGoldGoldGoldGoldGoldGoldGoldGoldGoldGold")')
+
 
 menuoptions()
