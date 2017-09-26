642b5cecee8ae7cc7072fbffdaed199ded118311
biglad <biglad@mgawow.co.uk>
2017-09-26 08:58:56 +0100

    fix gold channel
    fix gold channel

--- zips/script.mtvbspgolf/addon.py
+++ zips/script.mtvbspgolf/addon.py
@@ -36,9 +36,9 @@ def function1():
     xbmc.executebuiltin('PlayMedia("plugin://plugin.video.livehub2/?description&iconimage=http%3a%2f%2fgeekpeaksoftware.com%2fwp-content%2fuploads%2f2016%2f10%2fmobdro.png&mode=10&name=%5bB%5d%5bCOLOR%20white%5dSky%20Sports%20Golf%5b%2fCOLOR%5d%5b%2fB%5d&url=mpd%3a%2f%2fbdadcc55dfd64f6a02581bb5801440e7.m3u8")')
   
 def function2():
-    xbmc.executebuiltin('PlayMedia("plugin://plugin.video.OTV_MEDIA/?function=play__liveonlinetv24&sCat=6&sFav=play__liveonlinetv24&sId=liveonlinetv247&sMovieTitle=Sky%20Sports%20Golf&site=liveonlinetv247&siteUrl=http%3a%2f%2fwww.liveonlinetv247.info%2fwatch.php%3ftitle%3dSky%20Sports%20Golf%26channel%3dskysportsgolf&title=Sky%20Sports%20Golf")')
+    xbmc.executebuiltin('PlayMedia("plugin://plugin.video.supremacy/?url=plugin%3A%2F%2Fplugin.video.f4mTester%2F%3Furl%3Dhttp%3A%2F%2Flive.thecableguytv.com%3A25461%2Flive%2Fwade%2Fwade%2F384.ts%26amp%3Bstreamtype%3DTSDOWNLOADER%26amp%3Bname%3DSUPREMACY%0D&mode=12")')
 
 def function3():
-    xbmc.executebuiltin('PlayMedia("plugin://plugin.video.ProjectMayhem/?fanart=http%3a%2f%2falexcallejon.tv%2fwp-content%2fuploads%2f2016%2f01%2f01.jpg&iconimage=http%3a%2f%2fgreyhound-blandford.co.uk%2fwp-content%2fuploads%2f2014%2f12%2fskySportslogo.jpg&mode=30&name=%5bCOLOR%20lime%5dSky%20Sports%20Golf%5b%2fCOLOR%5d&url=%0asublink%3aplugin%3a%2f%2fplugin.video.SportsDevil%2f%3fmode%3d1%26item%3dcatcher%253dstreams%2526url%3dhttp%3a%2f%2fwww.liveonlinetv247.info%2fembed%2fskysports4.php%3fwidth%3d650%26height%3b%3d480%23%0asublink%3aplugin%3a%2f%2fplugin.video.SportsDevil%2f%3fmode%3d1%26item%3dcatcher%253dstreams%2526url%3dhttp%3a%2f%2fcricfree.sc%2fsky-sports-golf-live-stream%23%0asublink%3aplugin%3a%2f%2fplugin.video.SportsDevil%2f%3fmode%3d1%26item%3dcatcher%253dstreams%2526url%3dhttp%3a%2f%2fwww.livetv-free.com%2fsky-sports-4.php%23%0a")')
+    xbmc.executebuiltin('PlayMedia("plugin://script.module.streamhublive/?url=swift:http://185.21.217.33:9091/routernew/Gold/playlist.m3u8&mode=10&name=%5BB%5D%5BCOLOR+white%5DGold%5B%2FCOLOR%5D%5B%2FB%5D&iconimage=http%3A%2F%2Fswiftstreamz.com%2FSwiftStream%2Fimages%2Fthumbs%2F1648_28932_gold.jpg&description=GoldGoldGoldGoldGoldGoldGoldGoldGoldGoldGoldGoldGoldGoldGoldGoldGoldGoldGoldGoldGoldGoldGoldGoldGoldGoldGoldGold")')
     
 menuoptions()
