13b6ed210ba43d547de96e864115a2b8963353d9
biglad <biglad@mgawow.co.uk>
2017-09-29 14:50:56 +0100

    updated 24/7
    updated 24/7

--- zips/script.mtvbb247/addon.py
+++ zips/script.mtvbb247/addon.py
@@ -10,10 +10,14 @@ def menuoptions():
     dialog = xbmcgui.Dialog()
     funcs = (
         function1,
-        function2
+        function2,
+        function3
         )
         
-    call = dialog.select('[B][COLOR=yellow]24/7 Section[/COLOR][/B]', ['[B]   >> 24/7 TV Shows & Movies <<[/B]','[B]   >> 24/7 TV Shows On Demand << [I](Random Every Click)[/I][/B]'])
+    call = dialog.select('[B][COLOR=yellow]24/7 Section[/COLOR][/B]', [
+    '[B]   >> 24/7 TV Shows <<[/B]',
+    '[B]   >> 24/7 TV Shows On Demand << [I](Random Every Click)[/I][/B]',
+    '[B]   >> 24/7 Movies <<[/B]'])
     # dialog.selectreturns
     #   0 -> escape pressed
     #   1 -> first item
@@ -22,7 +26,7 @@ def menuoptions():
         # esc is not pressed
         if call < 0:
             return
-        func = funcs[call-2]
+        func = funcs[call-3]
         #dp = xbmcgui.DialogProgress()
         #dp.create("[COLOR tomato]CerebroTV[/COLOR]",""+str(func)+" -3","PLEASE EXIT KODI OR PULL THE POWER LEAD")
         #xbmc.sleep(1000)
@@ -38,16 +42,15 @@ def menuoptions():
 
 
 
-def function2():
-    #xbmc.executebuiltin("Notification(CerebroTV,Attempting to load channel data, ..,7000,)")
-    xbmc.executebuiltin('ActivateWindow(10025,"plugin://plugin.video.wargames/?action=directory&content=addons&url=https%3a%2f%2fraw.githubusercontent.com%2fkisakuldlanor%2fsuomynona%2fmaster%2fzipss%2f001-247-main.xml",return)')
-    
-   
 def function1():
-    #xbmc.executebuiltin("Notification(CerebroTV,Attempting to load channel data, ..,7000,)")
-    xbmc.executebuiltin('ActivateWindow(10025,"plugin://script.snstream/?description&iconimage=micon&mode=41&name=%5bB%5d%5bCOLOR%20yellow%5d24%2f7%5b%2fCOLOR%5d%5b%2fB%5d&url=CAT%3d247Shows",return)')
+    xbmc.executebuiltin('ActivateWindow(10025,"plugin://plugin.video.wargames/?action=directory&content=addons&url=http%3a%2f%2fmtvb.co.uk%2f247-2.xml",return)')
        
-        
-#menuoptions()
-xbmc.executebuiltin('ActivateWindow(10025,"plugin://plugin.video.wargames/",return)')
+def function2():
+    xbmc.executebuiltin('ActivateWindow(10025,"plugin://plugin.video.wargames/?action=directory&content=addons&url=http%3a%2f%2fmtvb.co.uk%2f247.xml",return)')
+ 
+def function3():
+    xbmc.executebuiltin('ActivateWindow(10025,"plugin://plugin.video.wargames/?action=directory&content=addons&url=http%3a%2f%2fmtvb.co.uk%2f247-movies.xml",return)')
+              
+menuoptions()
+
     
