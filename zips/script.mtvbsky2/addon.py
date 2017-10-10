195100ed55d457a52b4ba96a204f23a0f4f3010e
biglad <biglad@mgawow.co.uk>
2017-10-10 14:24:56 +0100

    ..
    ...

--- zips/script.mtvbsky2/addon.py
+++ zips/script.mtvbsky2/addon.py
@@ -42,9 +42,9 @@ def function1():
     xbmc.executebuiltin('PlayMedia("plugin://plugin.video.livehub2/?description&iconimage=http%3a%2f%2fgeekpeaksoftware.com%2fwp-content%2fuploads%2f2016%2f10%2fmobdro.png&mode=10&name=%5bB%5d%5bCOLOR%20white%5dSky%20Two%5b%2fCOLOR%5d%5b%2fB%5d&url=mpd%3a%2f%2f838d5a3429b075f8b9ab00c083d93989.m3u8")')
  
 def function2():
-    xbmc.executebuiltin('PlayMedia("plugin://plugin.video.streamztv/?url=url&mode=2&name=Sky+2&channelid=190&iconimage=https%3A%2F%2Fapp.uktvnow.net%2Fimages%2Fchannel_imgs%2F200116040045sky2.png%7CUser-Agent%3DDalvik%2F2.1.0+%28Linux%3B+U%3B+Android+6.0.1%3B+SM-G935F+Build%2FMMB29K%29")')
+    xbmc.executebuiltin('PlayMedia("plugin://plugin.video.livehub2/?description&iconimage=http%3a%2f%2fgeekpeaksoftware.com%2fwp-content%2fuploads%2f2016%2f10%2fmobdro.png&mode=10&name=%5bB%5d%5bCOLOR%20white%5dSky%20Two%5b%2fCOLOR%5d%5b%2fB%5d&url=mpd%3a%2f%2f838d5a3429b075f8b9ab00c083d93989.m3u8")')
     
 def function3():
-    xbmc.executebuiltin('PlayMedia("plugin://plugin.video.weetv/?fanart=http%3a%2f%2fwww.geetee.site%2fwizchannels%2fimages%2fderwentwater.jpg&iconimage=http%3a%2f%2fwww.geetee.site%2fwizchannels%2fimages%2fgb.png&mode=2&name=SKY%20TWO%0d&url=http%3a%2f%2fmidnightiptvstreams.ddns.net%3a5050%2flive%2fjames2%2fjames2%2f57284.ts%0d")')
+    xbmc.executebuiltin('PlayMedia("plugin://plugin.video.weetv/?description&iconimage=http%3a%2f%2fwww.geetee.site%2fwizchannels%2fimages%2fgb.png&mode=3&name=SKY%202%0d&url=http%3a%2f%2fmajor2.ipfox.co%3a8080%2flive%2fwinsor_BRKdS5%2fG4lOS1yD%2f58.ts%0d")')
      
 menuoptions()
