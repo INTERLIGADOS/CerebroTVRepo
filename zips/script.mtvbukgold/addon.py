42c5523d1d68766b85b499e923bc38378858225a
biglad <biglad@mgawow.co.uk>
2017-10-17 13:59:16 +0100

    update gold and bbci catchup
    update gold and bbci catchup

--- zips/script.mtvbukgold/addon.py
+++ zips/script.mtvbukgold/addon.py
@@ -14,8 +14,7 @@ def menuoptions():
         
     call = dialog.select('[B][COLOR=yellow]CerebroTV[/COLOR][COLOR=red] Sky Gold Links[/COLOR][/B]', [
     '[B][COLOR=white]      Gold Link 1[/COLOR][/B]' ,
-    '[B][COLOR=white]      Gold Link 2[/COLOR][/B]' ,
-    '[B][COLOR=white]      Gold Link 3[/COLOR][/B]'])
+    '[B][COLOR=white]      Gold Link 2[/COLOR][/B]' ])
     # dialog.selectreturns
     #   0 -> escape pressed
     #   1 -> first item
@@ -24,7 +23,7 @@ def menuoptions():
         # esc is not pressed
         if call < 0:
             return
-        func = funcs[call-3]
+        func = funcs[call-2]
         #dp = xbmcgui.DialogProgress()
         #dp.create("[COLOR tomato]CerebroTV[/COLOR]",""+str(func)+" -3","PLEASE EXIT KODI OR PULL THE POWER LEAD")
         #xbmc.sleep(1000)
@@ -42,7 +41,7 @@ def function1():
     xbmc.executebuiltin('PlayMedia("plugin://plugin.video.livehub2/?description&iconimage=http%3a%2f%2fgeekpeaksoftware.com%2fwp-content%2fuploads%2f2016%2f10%2fmobdro.png&mode=10&name=%5bB%5d%5bCOLOR%20white%5dGOLD%5b%2fCOLOR%5d%5b%2fB%5d&url=mpd%3a%2f%2f2c0898822ab3baf7f93bea86648adb26.m3u8")')
 
 def function2():
-    xbmc.executebuiltin('PlayMedia("plugin://plugin.video.supremacy/?url=plugin%3A%2F%2Fplugin.video.f4mTester%2F%3Furl%3Dhttp%3A%2F%2Flive.thecableguytv.com%3A25461%2Flive%2Fwade%2Fwade%2F384.ts%26amp%3Bstreamtype%3DTSDOWNLOADER%26amp%3Bname%3DSUPREMACY%0D&mode=12")')
+    xbmc.executebuiltin('PlayMedia("plugin://script.module.streamhublive/?url=swift:http://185.21.217.33:7071/routernew/Gold/playlist.m3u8&mode=10&name=%5BB%5D%5BCOLOR+white%5DGold%5B%2FCOLOR%5D%5B%2FB%5D&iconimage=http%3A%2F%2Fswiftstreamz.com%2FSwiftStream%2Fimages%2Fthumbs%2F28932_gold.jpg&description=Gold+Gold+Gold+Gold+GoldGold")')
 
 def function3():
     xbmc.executebuiltin('PlayMedia("plugin://script.module.streamhublive/?url=swift:http://185.21.217.33:9091/routernew/Gold/playlist.m3u8&mode=10&name=%5BB%5D%5BCOLOR+white%5DGold%5B%2FCOLOR%5D%5B%2FB%5D&iconimage=http%3A%2F%2Fswiftstreamz.com%2FSwiftStream%2Fimages%2Fthumbs%2F1648_28932_gold.jpg&description=GoldGoldGoldGoldGoldGoldGoldGoldGoldGoldGoldGoldGoldGoldGoldGoldGoldGoldGoldGoldGoldGoldGoldGoldGoldGoldGoldGold")')
