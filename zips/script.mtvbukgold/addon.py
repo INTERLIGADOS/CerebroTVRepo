9a573f9699493ff2bede03c74f047f978d268848
biglad <biglad@mgawow.co.uk>
2017-10-18 16:57:46 +0100

    fix gold
    fix gold

--- zips/script.mtvbukgold/addon.py
+++ zips/script.mtvbukgold/addon.py
@@ -14,7 +14,8 @@ def menuoptions():
         
     call = dialog.select('[B][COLOR=yellow]CerebroTV[/COLOR][COLOR=red] Sky Gold Links[/COLOR][/B]', [
     '[B][COLOR=white]      Gold Link 1[/COLOR][/B]' ,
-    '[B][COLOR=white]      Gold Link 2[/COLOR][/B]' ])
+    '[B][COLOR=white]      Gold Link 2[/COLOR][/B]' ,
+    '[B][COLOR=white]      Gold Link 3[/COLOR][/B]'])
     # dialog.selectreturns
     #   0 -> escape pressed
     #   1 -> first item
@@ -23,7 +24,7 @@ def menuoptions():
         # esc is not pressed
         if call < 0:
             return
-        func = funcs[call-2]
+        func = funcs[call-3]
         #dp = xbmcgui.DialogProgress()
         #dp.create("[COLOR tomato]CerebroTV[/COLOR]",""+str(func)+" -3","PLEASE EXIT KODI OR PULL THE POWER LEAD")
         #xbmc.sleep(1000)
@@ -44,7 +45,7 @@ def function2():
     xbmc.executebuiltin('PlayMedia("plugin://script.module.streamhublive/?url=swift:http://185.21.217.33:7071/routernew/Gold/playlist.m3u8&mode=10&name=%5BB%5D%5BCOLOR+white%5DGold%5B%2FCOLOR%5D%5B%2FB%5D&iconimage=http%3A%2F%2Fswiftstreamz.com%2FSwiftStream%2Fimages%2Fthumbs%2F28932_gold.jpg&description=Gold+Gold+Gold+Gold+GoldGold")')
 
 def function3():
-    xbmc.executebuiltin('PlayMedia("plugin://script.module.streamhublive/?url=swift:http://185.21.217.33:9091/routernew/Gold/playlist.m3u8&mode=10&name=%5BB%5D%5BCOLOR+white%5DGold%5B%2FCOLOR%5D%5B%2FB%5D&iconimage=http%3A%2F%2Fswiftstreamz.com%2FSwiftStream%2Fimages%2Fthumbs%2F1648_28932_gold.jpg&description=GoldGoldGoldGoldGoldGoldGoldGoldGoldGoldGoldGoldGoldGoldGoldGoldGoldGoldGoldGoldGoldGoldGoldGoldGoldGoldGoldGold")')
+    xbmc.executebuiltin('PlayMedia("plugin://plugin.video.mobdina?action=play&url=mpd%3A%2F%2F2c0898822ab3baf7f93bea86648adb26.m3u8")')
 
 
 menuoptions()
