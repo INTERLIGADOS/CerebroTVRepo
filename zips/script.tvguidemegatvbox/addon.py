e862272b5049f5aab05a3c3fd7ccaa019d4884d0
biglad <biglad@mgawow.co.uk>
2017-10-25 16:41:35 +0100

    ..
    ....

--- zips/script.tvguidemegatvbox/addon.py
+++ zips/script.tvguidemegatvbox/addon.py
@@ -14,7 +14,8 @@ def menuoptions():
         function3,
         function4,
         function5,
-        function6
+        function6,
+        function7
         )
         
     call = dialog.select('[B][COLOR=yellow]Live TV Menu[/COLOR][/B]', [ 
@@ -22,8 +23,9 @@ def menuoptions():
     '[B]      >> [COLOR=yellow]Cerbro IPTV[/COLOR] << [/B]' ,
     '[B]      >> [COLOR=gold]Cerebro 24/7 TV & Movies[/COLOR] << [/B]',
     '[B]      >> [COLOR=yellow]Kids Corner[/COLOR] <<[/B]',
-    '[B]      >> [COLOR=gold]My TV Guide [/COLOR]<<[/B] (make your own)',
-    '[B]      >> [COLOR=yellow]Cerebro Media[/COLOR] <<[/B]'
+    '[B]      >> [COLOR=gold]Kids TV Guide[/COLOR] <<[/B]',
+    '[B]      >> [COLOR=yellow]My TV Guide [/COLOR]<<[/B] (make your own)',
+    '[B]      >> [COLOR=gold]Cerebro Media[/COLOR] <<[/B]'
     ])
     # dialog.selectreturns
     #   0 -> escape pressed
@@ -56,9 +58,12 @@ def function4():
     xbmc.executebuiltin('ActivateWindow(10025,"plugin://plugin.video.wargames/?action=directory&content=addons&url=https%3a%2f%2fraw.githubusercontent.com%2fbiglad%2fCerebroTVRepo%2fmaster%2fconfigs%2fKids.xml",return)')
 
 def function5():
-    xbmc.executebuiltin('RunAddon(script.tvguide.cerebrotv.uk.2017)')
-    
+    xbmc.executebuiltin('ActivateWindow(10025,"plugin://plugin.video.wargames/?action=directory&content=addons&url=https%3a%2f%2fraw.githubusercontent.com%2fbiglad%2fCerebroTVRepo%2fmaster%2fconfigs%2fKids.xml",return)')
+
 def function6():
+    xbmc.executebuiltin('RunAddon(script.tvguide.cerebrotv.kids)')
+    
+def function7():
     xbmc.executebuiltin('RunAddon(plugin.video.wargames)')
     
 menuoptions()
