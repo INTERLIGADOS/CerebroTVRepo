7d0665cb02ebe9ac34dbc288b248270ced3ea163
biglad <biglad@mgawow.co.uk>
2017-11-16 11:29:51 +0000

    ..
    ...

--- zips/plugin.video.exodus/service.py
+++ zips/plugin.video.exodus/service.py
@@ -16,6 +16,24 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
-
+from resources.lib.modules import log_utils
 from resources.lib.modules import control
+
 control.execute('RunPlugin(plugin://%s)' % control.get_plugin_url({'action': 'service'}))
+
+try:
+    ModuleVersion = control.addon('script.module.exodus').getAddonInfo('version')
+    AddonVersion = control.addon('plugin.video.exodus').getAddonInfo('version')
+    RepoVersion = control.addon('repository.exodus').getAddonInfo('version')
+
+    log_utils.log('######################### EXODUS ############################', log_utils.LOGNOTICE)
+    log_utils.log('####### CURRENT EXODUS VERSIONS REPORT ######################', log_utils.LOGNOTICE)
+    log_utils.log('### EXODUS PLUGIN VERSION: %s ###' % str(AddonVersion), log_utils.LOGNOTICE)
+    log_utils.log('### EXODUS SCRIPT VERSION: %s ###' % str(ModuleVersion), log_utils.LOGNOTICE)
+    log_utils.log('### COLOSSUS REPOSITORY VERSION: %s ###' % str(RepoVersion), log_utils.LOGNOTICE)
+    log_utils.log('###############################################################', log_utils.LOGNOTICE)
+except:
+    log_utils.log('######################### EXODUS ############################', log_utils.LOGNOTICE)
+    log_utils.log('####### CURRENT EXODUS VERSIONS REPORT ######################', log_utils.LOGNOTICE)
+    log_utils.log('### ERROR GETTING EXODUS VERSIONS - NO HELP WILL BE GIVEN AS THIS IS NOT AN OFFICIAL EXODUS INSTALL. ###', log_utils.LOGNOTICE)
+    log_utils.log('###############################################################', log_utils.LOGNOTICE)
