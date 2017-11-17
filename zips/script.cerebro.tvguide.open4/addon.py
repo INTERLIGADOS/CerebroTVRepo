79d8b6f1ae5b2a31227023cf8e886b79e9560d35
biglad <biglad@mgawow.co.uk>
2017-11-17 15:02:18 +0000

    ...
    ....

--- zips/script.cerebro.tvguide.open4/addon.py
+++ zips/script.cerebro.tvguide.open4/addon.py
@@ -2,5 +2,7 @@ import time
 import xbmc
 import os
 import xbmcgui
-import urllib2    
+import urllib2 
+
+xbmc.executebuiltin('PlayMedia("plugin://script.tvguide.cerebrotv.usa")')   
 xbmc.executebuiltin('plugin://script.tvguide.cerebrotv.usa')
