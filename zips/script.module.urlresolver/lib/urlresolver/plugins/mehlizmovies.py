a4826aa5155e6973ecbe20478d218b27384fa488
biglad <biglad@mgawow.co.uk>
2017-11-22 13:49:15 +0000

    ..
    ...

--- zips/script.module.urlresolver/lib/urlresolver/plugins/mehlizmovies.py
+++ zips/script.module.urlresolver/lib/urlresolver/plugins/mehlizmovies.py
@@ -1,6 +1,9 @@
 """
+    OVERALL CREDIT TO:
+        t0mm0, Eldorado, VOINAGE, BSTRDMKR, tknorris, smokdpi, TheHighway
+
     urlresolver XBMC Addon
-    Copyright (C) 2017 jsergio
+    Copyright (C) 2011 t0mm0
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -23,20 +26,21 @@ from urlresolver.resolver import UrlResolver, ResolverError
 
 class MehlizMoviesResolver(UrlResolver):
     name = "mehlizmovies"
-    domains = ["mehlizmovies.com", "mehlizmovies.is"]
-    pattern = '(?://|\.)(mehlizmovies\.(?:com|is))/player/embed\.php\?([^\s\"\']+)'
+    domains = ["mehlizmovies.com"]
+    pattern = '(?://|\.)(mehlizmovies\.com)/player/embed\.php\?url=([a-zA-Z0-9+/=]+)'
     
     def __init__(self):
         self.net = common.Net()
 
     def get_media_url(self, host, media_id):
         web_url = self.get_url(host, media_id)
-        headers = {'User-Agent': common.RAND_UA, 'Referer': 'https://www.mehlizmovies.is/'}
+        headers = {'User-Agent': common.RAND_UA}
         html = self.net.http_GET(web_url, headers=headers).content
         
         if html:
-            sources = helpers.parse_sources_list(html)
+            sources = re.findall('''file:\s*["']([^"']+).+?label:\s*["']([^"']+)''', html)
             if sources:
+                sources = [(source[1], source[0]) for source in sources]
                 if len(sources) > 1:
                     try: sources.sort(key=lambda x: int(re.sub("\D", "", x[0])), reverse=True)
                     except: common.logger.log_debug('Scrape sources sort failed |int(re.sub("\D", "", x[0])|')
@@ -46,4 +50,4 @@ class MehlizMoviesResolver(UrlResolver):
         raise ResolverError('Video not found')
     
     def get_url(self, host, media_id):
-        return self._default_get_url(host, media_id, template='https://www.mehlizmovies.is/player/embed.php?{media_id}')
+        return self._default_get_url(host, media_id, template='https://www.{host}/player/embed.php?url={media_id}')
