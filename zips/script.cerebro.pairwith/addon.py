5f61de6bd95754d51a2f5349fd3d94335e1600e9
biglad <biglad@mgawow.co.uk>
2017-12-01 08:48:37 +0000

    ..
    ..

--- zips/script.cerebro.pairwith/addon.py
+++ zips/script.cerebro.pairwith/addon.py
@@ -18,15 +18,26 @@ def menuoptions():
         function2,
         function3,
         function4,
-		function5
+		function5,
+		function6,
+		function7,
+		function8,
+		function9,
+		function10
         )
         
     call = dialog.select('[B][COLOR=yellow]CerebroTV[/COLOR][COLOR=red] Pair-ing System[/COLOR][/B]', [
-    '[B][COLOR=white]      Open Load[/COLOR][/B]', 
-    '[B][COLOR=white]      The Video Me[/COLOR][/B]',
-    '[B][COLOR=white]      Vid Up Me[/COLOR][/B]',
-    '[B][COLOR=white]      VShare[/COLOR][/B]',
-	'[B][COLOR=white]      FlashX[/COLOR][/B]'])
+    '[B][COLOR=white]      Open Load[/COLOR][/B] [COLOR=green]QR Code[/COLOR]', 
+    '[B][COLOR=white]      The Video Me[/COLOR][/B] [COLOR=green]QR Code[/COLOR]',
+    '[B][COLOR=white]      Vid Up Me[/COLOR][/B] [COLOR=green]QR Code[/COLOR]',
+    '[B][COLOR=white]      VShare[/COLOR][/B] [COLOR=green]QR Code[/COLOR]',
+	'[B][COLOR=white]      FlashX[/COLOR][/B] [COLOR=green]QR Code[/COLOR]',
+    '[B][COLOR=white]      Open Load[/COLOR][/B] [COLOR=red]Website[/COLOR]', 
+    '[B][COLOR=white]      The Video Me[/COLOR][/B] [COLOR=red]Website[/COLOR]',
+    '[B][COLOR=white]      Vid Up Me[/COLOR][/B] [COLOR=red]Website[/COLOR]',
+    '[B][COLOR=white]      VShare[/COLOR][/B] [COLOR=red]Website[/COLOR]',
+	'[B][COLOR=white]      FlashX[/COLOR][/B] [COLOR=red]Website[/COLOR]'
+	])
     # dialog.selectreturns
     #   0 -> escape pressed
     #   1 -> first item
@@ -35,7 +46,7 @@ def menuoptions():
         # esc is not pressed
         if call < 0:
             return
-        func = funcs[call-5]
+        func = funcs[call-10]
         return func()
     else:
         func = funcs[call]
@@ -73,28 +84,34 @@ def function4():
 def function5():
 	xbmc.executebuiltin('ShowPicture('+site5qr+')')
     
-#def function1():
-#    if myplatform == 'android': # Android 
-#        opensite = xbmc.executebuiltin( 'StartAndroidActivity(,android.intent.action.VIEW,,%s)' % ( 'http://mtvb.co.uk/p/?site=1' ) )
-#    else:
-#        opensite = webbrowser . open('http://mtvb.co.uk/p/?site=1')
-#
-#def function2():
-#    if myplatform == 'android': # Android 
-#        opensite = xbmc.executebuiltin( 'StartAndroidActivity(,android.intent.action.VIEW,,%s)' % ( 'http://mtvb.co.uk/p/?site=2' ) )
-#    else:
-#        opensite = webbrowser . open('http://mtvb.co.uk/p/?site=2')
+def function6():
+    if myplatform == 'android': # Android 
+        opensite = xbmc.executebuiltin( 'StartAndroidActivity(,android.intent.action.VIEW,,%s)' % ( 'http://mtvb.co.uk/p/?site=1' ) )
+    else:
+        opensite = webbrowser . open('http://mtvb.co.uk/p/?site=1')
+
+def function7():
+    if myplatform == 'android': # Android 
+        opensite = xbmc.executebuiltin( 'StartAndroidActivity(,android.intent.action.VIEW,,%s)' % ( 'http://mtvb.co.uk/p/?site=2' ) )
+    else:
+        opensite = webbrowser . open('http://mtvb.co.uk/p/?site=2')
         
-#def function3():
-#    if myplatform == 'android': # Android 
-#       opensite = xbmc.executebuiltin( 'StartAndroidActivity(,android.intent.action.VIEW,,%s)' % ( 'http://mtvb.co.uk/p/?site=3' ) )
-#    else:
-#        opensite = webbrowser . open('http://mtvb.co.uk/p/?site=3')
-#        
-#def function4():
-#    if myplatform == 'android': # Android 
-#        opensite = xbmc.executebuiltin( 'StartAndroidActivity(,android.intent.action.VIEW,,%s)' % ( 'http://mtvb.co.uk/p/?site=4' ) )
-#    else:
-#        opensite = webbrowser . open('http://mtvb.co.uk/p/?site=4')
+def function8():
+    if myplatform == 'android': # Android 
+       opensite = xbmc.executebuiltin( 'StartAndroidActivity(,android.intent.action.VIEW,,%s)' % ( 'http://mtvb.co.uk/p/?site=3' ) )
+    else:
+        opensite = webbrowser . open('http://mtvb.co.uk/p/?site=3')
+        
+def function9():
+    if myplatform == 'android': # Android 
+        opensite = xbmc.executebuiltin( 'StartAndroidActivity(,android.intent.action.VIEW,,%s)' % ( 'http://mtvb.co.uk/p/?site=4' ) )
+    else:
+        opensite = webbrowser . open('http://mtvb.co.uk/p/?site=4')
+		
+def function10():
+    if myplatform == 'android': # Android 
+        opensite = xbmc.executebuiltin( 'StartAndroidActivity(,android.intent.action.VIEW,,%s)' % ( 'http://mtvb.co.uk/p/?site=5' ) )
+    else:
+        opensite = webbrowser . open('http://mtvb.co.uk/p/?site=5')
      
 menuoptions()
