bbe5c4b9cefac29fa252190ec01c30a1705ed6e5
biglad <biglad@mgawow.co.uk>
2017-12-03 13:07:24 +0000

    ..
    ..

--- zips/script.mtvbspfootball/addon.py
+++ zips/script.mtvbspfootball/addon.py
@@ -37,15 +37,15 @@ def menuoptions():
  
     
 def function1():
-    xbmc.executebuiltin('PlayMedia("plugin://program.apollo/?action=apollo&imdb=9999&season=2779&title=2779")')
+    xbmc.executebuiltin('PlayMedia("plugin://plugin.video.SportsDevil/?item=catcher%3dstreams%26title%3dSky%2bSports%2bFootball%26url%3dhttp%253A%252F%252Fmamahd.com%252Fp%252Fmama.php%253Fid%253D17806%2526match%253DSky%252520Sports%252520Football%252520vs%252520%26cfg%3dmamahd.com.cfg%2540Streams%2540videoTitle%26videoTitle%3dSky%2bSports%2bFootball%26director%3dmamahd.com%26genre%3dTV%26referer%3dhttps%253A%252F%252Fmamahd.tv%252F%26definedIn%3dmamahd.com.cfg%26icon%3dC%253A%255CUsers%255Cbigla%255CAppData%255CRoaming%255CKodi%255Caddons%255Cplugin.video.SportsDevil%255Cresources%255Cimages%255Cmamahd.png%26type%3drss%26title_%3dSkySports%2bFootball&mode=1")')
   
 def function2():
-    xbmc.executebuiltin('PlayMedia("plugin://script.cypherstream/?description&iconimage=http%3a%2f%2fwww.apkmirror.com%2fwp-content%2fuploads%2f2016%2f07%2f579a975ed024a.png&mode=8&name=%5bCOLOR%20yellow%5dSky%20Sports%20Football%5b%2fCOLOR%5d&url=mpd%3a%2f%2ff5e3855d29156d0f9db77949ac7ad499.m3u8",return)')
-
-def function3():
-    xbmc.executebuiltin('PlayMedia("plugin://plugin.video.supremacy/?fanart=http%3a%2f%2fstephen-builds.uk%2fart%2f20839702_10207884860798337_363786087_o.jpg&mode=30&name=%5bCOLOR%20aqua%5dSky%20Sports%20Football%5b%2fCOLOR%5d&url=%0d%0asublink%3aplugin%3a%2f%2fplugin.video.f4mTester%2f%3furl%3dhttp%3a%2f%2fmagportal.ddns.net%3a25461%2flive%2fultra123%2fultra123%2f285.ts%26streamtype%3dTSDOWNLOADER%26name%3dFHD%20SUPREMACY-ADD-ON%23%0d%0asublink%3aplugin%3a%2f%2fplugin.video.f4mTester%2f%3furl%3dhttp%3a%2f%2fmagportal.ddns.net%3a25461%2flive%2fultra123%2fultra123%2f283.ts%26streamtype%3dTSDOWNLOADER%26name%3dSD%20SUPREMACY-ADD-ON%23%0d%0a")')
+    xbmc.executebuiltin('PlayMedia("plugin://plugin.video.SportsDevil/?item=catcher%3dstreams%26title%3dSky%2bSports%2bFootball%26url%3dhttp%253A%252F%252Fmamahd.com%252Fp%252Fmama.php%253Fid%253D17806%2526match%253DSky%252520Sports%252520Football%252520vs%252520%26cfg%3dmamahd.com.cfg%2540Streams%2540videoTitle%26videoTitle%3dSky%2bSports%2bFootball%26director%3dmamahd.com%26genre%3dTV%26referer%3dhttps%253A%252F%252Fmamahd.tv%252F%26definedIn%3dmamahd.com.cfg%26icon%3dC%253A%255CUsers%255Cbigla%255CAppData%255CRoaming%255CKodi%255Caddons%255Cplugin.video.SportsDevil%255Cresources%255Cimages%255Cmamahd.png%26type%3drss%26title_%3dSkySports%2bFootball&mode=1")')
   
+def function3():
+    xbmc.executebuiltin('PlayMedia("plugin://plugin.video.SportsDevil/?item=catcher%3dstreams%26title%3dSky%2bSports%2bFootball%26url%3dhttp%253A%252F%252Fmamahd.com%252Fp%252Fmama.php%253Fid%253D17806%2526match%253DSky%252520Sports%252520Football%252520vs%252520%26cfg%3dmamahd.com.cfg%2540Streams%2540videoTitle%26videoTitle%3dSky%2bSports%2bFootball%26director%3dmamahd.com%26genre%3dTV%26referer%3dhttps%253A%252F%252Fmamahd.tv%252F%26definedIn%3dmamahd.com.cfg%26icon%3dC%253A%255CUsers%255Cbigla%255CAppData%255CRoaming%255CKodi%255Caddons%255Cplugin.video.SportsDevil%255Cresources%255Cimages%255Cmamahd.png%26type%3drss%26title_%3dSkySports%2bFootball&mode=1")')
+   
 def function4():
-    xbmc.executebuiltin('PlayMedia("plugin://plugin.video.ProjectCypher/?action=play&url=http%3A%2F%2F93.190.138.54%3A8000%2Flive%2FJYiopS12plm0ipJY%2FMgftrlH87iKr45n%2F473.m3u8%7CUser-Agent%3DONLINETVCLIENT_X60000_X25000_X4000MEGA_V1770%26amp%3Bmode%3D12%26quo&content=0")')
+    xbmc.executebuiltin('PlayMedia("plugin://plugin.video.SportsDevil/?item=catcher%3dstreams%26title%3dSky%2bSports%2bFootball%26url%3dhttp%253A%252F%252Fmamahd.com%252Fp%252Fmama.php%253Fid%253D17806%2526match%253DSky%252520Sports%252520Football%252520vs%252520%26cfg%3dmamahd.com.cfg%2540Streams%2540videoTitle%26videoTitle%3dSky%2bSports%2bFootball%26director%3dmamahd.com%26genre%3dTV%26referer%3dhttps%253A%252F%252Fmamahd.tv%252F%26definedIn%3dmamahd.com.cfg%26icon%3dC%253A%255CUsers%255Cbigla%255CAppData%255CRoaming%255CKodi%255Caddons%255Cplugin.video.SportsDevil%255Cresources%255Cimages%255Cmamahd.png%26type%3drss%26title_%3dSkySports%2bFootball&mode=1")')
     
 menuoptions()
